00bd0b312a8d3eb28c53732134dfa4d43dd494d2
Polisetti Dedeepya <polisettidedeepya313@gmail.com>
2025-12-08 19:04:39 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -168,4 +168,4 @@ async def download_output(filename: str):
     return FileResponse(path, media_type=media_type, filename=safe_name)
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
-    uvicorn.run(app, host="0.0.0.0", port=port)
+    uvicorn.run("main:app", host="0.0.0.0", port=port,reload=False)
